91ad6c8e6a1893a945fe5664234ce3d007d90a34
Renaud Hager <renaud.hager@gmail.com>
2017-02-10 18:24:34 +0000

    Add 2 rules : - allow outgoing trafic. - allow ICMP trafic.

--- security.tf
+++ security.tf
@@ -15,13 +15,28 @@ resource "aws_security_group" "bastion" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+  # Allow ICMP traffic
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${data.terraform_remote_state.vpc_rs.vpc_cidr_block}"]
+  }
+
+  # Allow outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name  = "${var.owner}_bastion"
     owner = "${var.owner}"
   }
 }
-
 
 #
 # Outputs
